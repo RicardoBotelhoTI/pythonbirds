9052656c8464e5e8a9a2c87e002632071cc60c4f
Ricardo Botelho <ricardo_patro@hotmail.com>
2022-07-25 15:52:05 -0300

    Criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa: # nome de classe tem que começar com letra maiscula se for mais de uma palavra a posteior também começa com letra maiscula ex: class PessoaCadastro:
-    def __init__(self, nome=None, idade=41):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar()) # Passando o elemento depois o metodo
-    print(p.nome)
-    p.nome = 'Ricardo'
-    print(p.nome)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar()) # Passando o elemento depois o metodo
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
